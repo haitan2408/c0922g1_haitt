ea319acd227b2bcf8de3aa47e2abc80fc72316f2
haitan2408 <haitan28102408@gmail.com>
2022-10-06 12:02:53 +0700

    up js

---
+++ module 1/ss6_js/demo_js.js
@@ -0,0 +1,10 @@
+alert("1")
+document.getElementById("c09").innerHTML="<b>Hello everybody</b>"
+alert("2")
+function callMe() {
+
+   document.write("hello C09");
+
+   // confirm("confirm call me?")
+   //  console.log("Warning")
+}
